0e736f1bd8d73b591ddc783d4b84bc4c0ad3d740
FerrumVega <ferrumthevega@gmail.com>
2025-07-23 12:23:21 +0300

    Мелкие исправления

---
+++ build.bat
@@ -0,0 +1,3 @@
+pyinstaller --add-data "background1.png;." --add-data "background.png;." --add-data "minecraft_title.ico;." --add-data "minecraft_title.png;." --icon=minecraft_title.ico --onefile --noconsole main.py
+
+PAUSE
---
+++ main.py
@@ -0,0 +1,460 @@
+import minecraft_launcher_lib
+import subprocess
+import os
+import requests
+import time
+import ctypes
+import threading
+import sys
+import sv_ttk
+import configparser
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+
+try:
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+except:  # noqa: E722
+    pass
+
+
+def catch_errors(func):
+    global start_button, progress_var, download_info
+
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except FileNotFoundError as e:
+            if e.winerror == 2:
+                messagebox.showerror(
+                    "Ошибка",
+                    f"При отсутствие java, скачайте её с github лаунчера\n\n{func.__name__}:\n{e}",
+                )
+        except Exception as e:
+            messagebox.showerror("Ошибка", f"Произошла ошибка в {func.__name__}:\n{e}")
+            try:
+                start_button["state"] = "normal"
+                progress_var.set(0)
+                download_info.set("Во время загрузки произошла ошибка.")
+            except:  # noqa: E722
+                pass
+
+    return wrapper
+
+
+@catch_errors
+def resolve_version_names(raw_version, mod_loader):
+    name_of_version_to_install = raw_version
+    name_of_version_folder = raw_version
+    if mod_loader == "forge":
+        if minecraft_launcher_lib.forge.find_forge_version(raw_version):
+            name_of_version_to_install = (
+                minecraft_launcher_lib.forge.find_forge_version(raw_version)
+            )
+            name_of_version_folder = f"{name_of_version_to_install.split("-")[0]}-forge-{name_of_version_to_install.split("-")[1]}"
+            return (name_of_version_to_install, name_of_version_folder)
+        else:
+            return None
+    elif mod_loader == "fabric":
+        if minecraft_launcher_lib.fabric.is_minecraft_version_supported(raw_version):
+            name_of_version_folder = f"fabric-loader-{minecraft_launcher_lib.fabric.get_latest_loader_version()}-{name_of_version_to_install}"
+            return (name_of_version_to_install, name_of_version_folder)
+        else:
+            return None
+
+    return (name_of_version_to_install, name_of_version_folder)
+
+
+@catch_errors
+def resource_path(relative_path):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.abspath(relative_path)
+
+
+@catch_errors
+def load_config():
+    default_config = {
+        "version": "1.16.5",
+        "mod_loader": "fabric",
+        "nickname": "Player",
+        "fix_mode": "0",
+        "java_arguments": "",
+        "sodium": "1",
+    }
+    file_path = "FVLauncher.ini"
+    parser = configparser.ConfigParser()
+
+    if not os.path.isfile(file_path):
+        parser.add_section("Settings")
+        parser["Settings"] = default_config
+        with open(file_path, "w", encoding="utf-8") as config_file:
+            parser.write(config_file)
+    else:
+        updated = False
+        parser.read(file_path, encoding="utf-8")
+        for key, value in default_config.items():
+            if key not in parser["Settings"]:
+                parser["Settings"][key] = value
+                updated = True
+        if updated:
+            with open(file_path, "w", encoding="utf-8") as config_file:
+                parser.write(config_file)
+
+    return {key: parser["Settings"][key] for key in parser.options("Settings")}
+
+
+@catch_errors
+def gui(
+    chosen_version,
+    chosen_mod_loader,
+    chosen_nickname,
+    fix_mode_position,
+    chosen_java_arguments,
+    sodium_position,
+):
+    global start_button, progress_var, download_info
+
+    @catch_errors
+    def safe_config():
+        settings = {
+            "version": version_var.get(),
+            "mod_loader": mod_loader_var.get(),
+            "nickname": nickname_var.get(),
+            "fix_mode": str(fix_mode_var.get()),
+            "java_arguments": java_arguments_var.get(),
+            "sodium": str(sodium_var.get()),
+        }
+        file_path = "FVLauncher.ini"
+        parser = configparser.ConfigParser()
+
+        parser.add_section("Settings")
+        parser["Settings"] = settings
+
+        with open(file_path, "w", encoding="utf-8") as config_file:
+            parser.write(config_file)
+        root.destroy()
+        os._exit(0)
+
+    @catch_errors
+    def block_sodium_checkbox(*args):
+        sodium_checkbox["state"] = (
+            "normal" if mod_loader_var.get() == "fabric" else "disabled"
+        )
+
+    @catch_errors
+    def on_start_button():
+        start_button["state"] = "disabled"
+        mod_loader = mod_loader_var.get()
+        raw_version = version_var.get()
+        nickname = nickname_var.get()
+        fix_mode = fix_mode_var.get()
+        java_arguments = java_arguments_var.get().split()
+        sodium = sodium_var.get()
+        returned_versions_data = resolve_version_names(raw_version, mod_loader)
+
+        if raw_version not in versions_names_list:
+            messagebox.showerror("Ошибка запуска", "Выберите версию из списка.")
+            start_button["state"] = "normal"
+        elif returned_versions_data:
+            version, version_name = returned_versions_data
+            if all((mod_loader, nickname, version)):
+                download_info_label.place(relx=0.5, y=430, anchor="center")
+                threading.Thread(
+                    target=launch,
+                    args=(
+                        mod_loader,
+                        nickname,
+                        version,
+                        version_name,
+                        fix_mode,
+                        java_arguments,
+                        start_button,
+                        progress_var,
+                        download_info,
+                        raw_version,
+                        sodium,
+                    ),
+                    daemon=True,
+                ).start()
+            else:
+                null_elements = ", ".join(
+                    [
+                        name
+                        for element, name in zip(
+                            (mod_loader, nickname, version),
+                            ("загрузчик модов", "никнейм", "версия"),
+                        )
+                        if not element
+                    ]
+                ).capitalize()
+                messagebox.showerror(
+                    "Ошибка запуска",
+                    f"Следующие поля не заполнены:\n{null_elements}.",
+                )
+                start_button["state"] = "normal"
+        else:
+            messagebox.showerror(
+                "Ошибка", "Для данной версии нет выбранного вами загрузчика модов."
+            )
+            start_button["state"] = "normal"
+
+    @catch_errors
+    def open_settings():
+        settings_window = tk.Toplevel()
+        settings_window.title("Настройки")
+        settings_window.iconbitmap(resource_path("minecraft_title.ico"))
+        root.iconphoto(True, tk.PhotoImage(file=resource_path("minecraft_title.png")))
+        settings_window.geometry(root.geometry())
+        settings_window.resizable(width=False, height=False)
+
+        settings_window.bg_image = tk.PhotoImage(file=resource_path("background.png"))
+        settings_bg_label = ttk.Label(settings_window, image=settings_window.bg_image)
+        settings_bg_label.place(relwidth=1, relheight=1)
+
+        java_arguments_label = ttk.Label(settings_window, text="java-аргументы")
+        java_arguments_label.place(relx=0.5, y=30, anchor="center")
+
+        java_arguments_entry = ttk.Entry(
+            settings_window, textvariable=java_arguments_var
+        )
+        java_arguments_entry.place(relx=0.5, y=60, anchor="center")
+
+        fix_mode_checkbox = ttk.Checkbutton(
+            settings_window, text="fix-mode", variable=fix_mode_var
+        )
+        fix_mode_checkbox.place(relx=0.5, y=110, anchor="center")
+
+    root = tk.Tk()
+    root.title("FVLauncher")
+    root.iconbitmap(resource_path("minecraft_title.ico"))
+    root.iconphoto(True, tk.PhotoImage(file=resource_path("minecraft_title.png")))
+    root.geometry("300x500")
+    root.resizable(width=False, height=False)
+
+    version_var = tk.StringVar()
+    version_var.set(chosen_version)
+
+    mod_loader_var = tk.StringVar()
+    mod_loader_var.set(chosen_mod_loader)
+
+    nickname_var = tk.StringVar()
+    nickname_var.set(chosen_nickname)
+
+    sodium_var = tk.IntVar()
+    sodium_var.set(sodium_position)
+
+    progress_var = tk.DoubleVar()
+
+    download_info = tk.StringVar()
+    download_info.set("Загрузка...")
+
+    java_arguments_var = tk.StringVar()
+    java_arguments_var.set(chosen_java_arguments)
+
+    fix_mode_var = tk.IntVar()
+    fix_mode_var.set(int(fix_mode_position))
+
+    bg_image = tk.PhotoImage(file=resource_path("background1.png"))
+    bg_label = ttk.Label(root, image=bg_image)
+    bg_label.place(relwidth=1, relheight=1)
+
+    versions_names_list = []
+    versions_list = minecraft_launcher_lib.utils.get_version_list()
+    for item in versions_list:
+        versions_names_list.append(item["id"])
+    versions_combobox = ttk.Combobox(
+        root, values=versions_names_list, textvariable=version_var
+    )
+    versions_combobox.place(x=80, y=30, anchor="center", relwidth=0.43)
+
+    mod_loaders = ["fabric", "forge", "vanilla"]
+    loaders_combobox = ttk.Combobox(
+        root, values=mod_loaders, textvariable=mod_loader_var
+    )
+    loaders_combobox.place(x=220, y=30, anchor="center", relwidth=0.43)
+
+    nickname_entry = ttk.Entry(root, textvariable=nickname_var)
+    nickname_entry.place(relx=0.5, y=70, anchor="center", relwidth=0.9)
+
+    sodium_checkbox = ttk.Checkbutton(root, text="Sodium", variable=sodium_var)
+    sodium_checkbox.place(relx=0.5, y=110, anchor="center", relwidth=0.9)
+    root.bind("<<ComboboxSelected>>", block_sodium_checkbox)
+
+    start_button = ttk.Button(root, text="Запустить игру", command=on_start_button)
+    start_button.place(relx=0.5, y=150, anchor="center", relwidth=0.9)
+
+    progressbar = ttk.Progressbar(root, variable=progress_var, length=295)
+    progressbar.place(relx=0.5, y=400, anchor="center")
+
+    download_info_label = ttk.Label(textvariable=download_info, font=("", 8))
+
+    settings_button = ttk.Button(root, text="⚙️", command=open_settings)
+    settings_button.place(x=270, y=480, anchor="center", relwidth=0.15)
+
+    sv_ttk.set_theme("dark")
+    root.protocol("WM_DELETE_WINDOW", safe_config)
+    root.mainloop()
+
+
+@catch_errors
+def prepare_installation_parameters(mod_loader, nickname, java_arguments):
+    if mod_loader == "fabric":
+        install_type = minecraft_launcher_lib.fabric.install_fabric
+    elif mod_loader == "forge":
+        install_type = minecraft_launcher_lib.forge.install_forge_version
+    else:
+        install_type = minecraft_launcher_lib.install.install_minecraft_version
+
+    options = {
+        "username": nickname,
+        "uuid": "a00a0aaa-0aaa-00a0-a000-00a0a00a0aa0",
+        "token": "",
+        "jvmArguments": java_arguments,
+    }
+    minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
+    return install_type, minecraft_directory, options
+
+
+@catch_errors
+def install_version(
+    version,
+    version_name,
+    install_type,
+    fix_mode,
+    minecraft_directory,
+    progress_var,
+    download_info,
+):
+    progress = 0
+    max_progress = 100
+
+    @catch_errors
+    def track_progress(value, type=None):
+        nonlocal progress, max_progress
+        if type == "progress":
+            progress = value
+        elif type == "max":
+            max_progress = value
+        else:
+            download_info.set(value)
+        try:
+            percents = progress / max_progress * 100
+        except ZeroDivisionError:
+            percents = 0
+        if percents > 100.0:
+            percents = 100.0
+        progress_var.set(percents)
+
+    if (
+        not os.path.isdir(os.path.join(minecraft_directory, "versions", version_name))
+        or fix_mode
+    ):
+        install_type(
+            version,
+            minecraft_directory,
+            callback={
+                "setProgress": lambda value: track_progress(value, "progress"),
+                "setMax": lambda value: track_progress(value, "max"),
+                "setStatus": lambda value: track_progress(value),
+            },
+        )
+    else:
+        download_info.set("Версия уже установлена, запуск...")
+
+
+def download_sodium(sodium_path, raw_version, download_info):
+    url = None
+    for sodium_version in requests.get(
+        "https://api.modrinth.com/v2/project/sodium/version"
+    ).json():
+        if raw_version in sodium_version["game_versions"]:
+            url = sodium_version["files"][0]["url"]
+            break
+    else:
+        messagebox.showwarning(
+            "Запуск без sodium", "Sodium недоступен на выбранной вами версии."
+        )
+    if url:
+        download_info.set("Загрузка Sodium...")
+        with open(sodium_path, "wb") as jar:
+            jar.write(requests.get(url).content)
+
+
+@catch_errors
+def launch(
+    mod_loader,
+    nickname,
+    version,
+    version_name,
+    fix_mode,
+    java_arguments,
+    start_button,
+    progress_var,
+    download_info,
+    raw_version,
+    sodium,
+):
+
+    install_type, minecraft_directory, options = prepare_installation_parameters(
+        mod_loader, nickname, java_arguments
+    )
+
+    install_version(
+        version,
+        version_name,
+        install_type,
+        fix_mode,
+        minecraft_directory,
+        progress_var,
+        download_info,
+    )
+    sodium_path = os.path.join(minecraft_directory, "mods", "sodium.jar")
+
+    if not os.path.isdir(os.path.join(minecraft_directory, "mods")):
+        os.mkdir(os.path.join(minecraft_directory, "mods"))
+
+    if sodium and mod_loader == "fabric":
+        download_sodium(sodium_path, raw_version, download_info)
+
+    elif os.path.isfile(sodium_path):
+        os.remove(sodium_path)
+
+    download_info.set("Версия установлена, запуск...")
+
+    minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(
+        version_name, minecraft_directory, options
+    )
+    start_button["state"] = "normal"
+    subprocess.run(
+        f'netsh advfirewall firewall add rule name="Block Minecraft" dir=out action=block program={minecraft_launcher_lib.runtime.get_executable_path("jre-legacy", minecraft_directory)} enable=yes',
+        shell=True,
+        creationflags=subprocess.CREATE_NO_WINDOW,
+    )
+
+    subprocess.Popen(
+        minecraft_command,
+        cwd=minecraft_directory,
+        creationflags=subprocess.CREATE_NO_WINDOW,
+    )
+    download_info.set("Игра запущена")
+    progress_var.set(100)
+
+    time.sleep(20)
+
+    subprocess.run(
+        'netsh advfirewall firewall delete rule name="Block Minecraft"',
+        shell=True,
+        creationflags=subprocess.CREATE_NO_WINDOW,
+    )
+
+
+config = load_config()
+gui(
+    config["version"],
+    config["mod_loader"],
+    config["nickname"],
+    config["fix_mode"],
+    config["java_arguments"],
+    config["sodium"],
+)
